b45ebbc74f30961f3a7573a8f98ba9bfa9ec27a0
karno-bh <sergey.mordeev@gmail.com>
2018-10-23 18:45:55 +0300

    George graph implementdation intermediate commit

--- src/main/java/il/ac/sce/ir/metric/open_calais/processor/OpenCalaisEntiryResolverTextProcessor.java
+++ src/main/java/il/ac/sce/ir/metric/open_calais/processor/OpenCalaisEntiryResolverTextProcessor.java
@@ -1,12 +1,13 @@
 package il.ac.sce.ir.metric.open_calais.processor;
 
+import entity_extractor.TextEntities;
 import il.ac.sce.ir.metric.core.data.Text;
 import il.ac.sce.ir.metric.core.processor.TextProcessor;
 
-public class OpenCalaisTextProcessor implements TextProcessor<String, String> {
+public class OpenCalaisEntiryResolverTextProcessor implements TextProcessor<String, TextEntities> {
 
     @Override
-    public Text<String> process(Text<String> data) {
+    public Text<TextEntities> process(Text<String> data) {
         return null;
     }
 }
