9ce02e739a4b9489bc7aad1a523dd60bd1605aa3
karno-bh <sergey.mordeev@gmail.com>
2018-11-10 10:34:17 +0200

    Intermediate commit. Test with example how to use java thread pool

---
+++ src/main/java/il/ac/sce/ir/metric/concrete_metric/rouge/reporter/PeerMultiModelMultithreadedReporter.java
@@ -0,0 +1,12 @@
+package il.ac.sce.ir.metric.concrete_metric.rouge.reporter;
+
+import il.ac.sce.ir.metric.core.reporter.Reporter;
+import il.ac.sce.ir.metric.core.reporter.file_system_reflection.ProcessedCategory;
+
+public class PeerMultiModelMultithreadedReporter implements Reporter {
+
+    @Override
+    public void report(ProcessedCategory processedCategory, String metric) {
+
+    }
+}
